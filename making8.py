0c528036b50d29a93d2c5b339aa2299c1f5676de
beriamrutha <22hr1a3208@mtieat.org>
2024-09-19 14:02:48 +0530

    Create making8.py

---
+++ making8.py
@@ -0,0 +1,27 @@
+'''
+There are 3 labs in the CSE department are L1, L2, and L3 with a seating capacity of x, y, and z respectively. One of the 3 labs has been allocated for ACE training. Out of the available labs, find the lab which has minimal seating capacity.
+Input format:
+Input consists of 3 integers and a string
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+The fourth input denotes the lab which is allocated for ACE training
+Output format:
+Print the minimal seating capacity lab amongst the available labs.
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+L3
+Sample Output:
+L1
+'''
+x = int(input())
+y = int(input())
+z = int(input())
+allocated_lab = input()
+lab_capacities = {'L1': x, 'L2': y, 'L3': z}
+del lab_capacities[allocated_lab]
+min_capacity_lab = min(lab_capacities, key=lab_capacities.get)
+print(min_capacity_lab)
